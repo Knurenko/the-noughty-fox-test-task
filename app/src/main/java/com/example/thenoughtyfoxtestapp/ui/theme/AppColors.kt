e16e05e6a9533796c2eb4784ea98ea86e0e23a57
Knurenko <bod93_93@mail.ru>
2024-03-05 15:28:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/thenoughtyfoxtestapp/ui/theme/AppColors.kt
@@ -0,0 +1,30 @@
+package com.example.thenoughtyfoxtestapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * @author Knurenko Bogdan 13.02.2024
+ */
+class AppColors(
+    val primary: Color,
+    val error: Color,
+    val background: Color,
+    val onPrimary: Color,
+    val onBackground: Color
+)
+
+val lightColors = AppColors(
+    primary = Color(0xFF673AB7),
+    error = Color(0xFFF44336),
+    background = Color(0xFFFFFFFF),
+    onPrimary = Color(0xFFFFFFFF),
+    onBackground = Color(0xFF000000)
+)
+
+val darkColors = AppColors(
+    primary = Color(0xFFFF9800),
+    error = Color(0xFFF44336),
+    background = Color(0xFF000000),
+    onPrimary = Color(0xFF000000),
+    onBackground = Color(0xFFFFFFFF)
+)
---
+++ app/src/main/java/com/example/thenoughtyfoxtestapp/ui/theme/AppTheme.kt
@@ -0,0 +1,33 @@
+package com.example.thenoughtyfoxtestapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+
+object AppTheme {
+    val colors: AppColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColors.current
+}
+
+private val LocalColors = staticCompositionLocalOf { lightColors }
+
+@Composable
+fun AppTheme(
+    isDarkMode: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val themedColors: AppColors = if (isDarkMode) darkColors else lightColors
+
+    CompositionLocalProvider(
+        LocalColors provides themedColors,
+    ) {
+        MaterialTheme {
+            content()
+        }
+    }
+}
